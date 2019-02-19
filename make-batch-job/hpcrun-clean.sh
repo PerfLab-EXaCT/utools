1a4d0dfea457a10c3b7b40bb26e7651b757250de
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2019-02-19 16:20:40 +0000

    update.
    git-svn-id: https://svn.pnl.gov/svn/hpcgroup/projects/modsim-tools@18837 4c623a4d-1e8e-0410-8a3e-ee3132d44425

---
+++ make-batch-job/hpcrun-clean.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#****************************************************************************
+# $HeadURL$
+# $Id$
+#
+# Nathan Tallent
+#****************************************************************************
+
+#****************************************************************************
+#
+#****************************************************************************
+
+toDir="0threads"
+
+cmd=$0
+
+#****************************************************************************
+
+if [[ $1 =~ -(-)?h(elp)? ]] ; then
+  cat  <<EOF
+  usage: ${cmd} <hpctoolkit-measurements-dir>
+
+  Retains only MPI rank data (moving thread data).
+EOF
+  exit
+fi
+
+
+arg_hpctkDir=$1
+
+if [[ ! -d ${arg_hpctkDir} ]] ; then
+    printf "bad <hpctoolkit-measurements-dir>: '${arg_hpctkDir}'\n"
+    exit 1
+fi
+
+#****************************************************************************
+
+src_path="${arg_hpctkDir}"
+dst_path="${arg_hpctkDir}/${toDir}"
+
+printf "${arg_hpctkDir}: Retaining only MPI rank data (moving thread data)\n"
+
+mkdir -p "${dst_path}"
+
+mv "${src_path}"/*-??????-00[1-9]-*.{hpcrun,hpctrace}     "${dst_path}" \
+    &> /dev/null
+mv "${src_path}"/*-??????-0[1-9][0-9]-*.{hpcrun,hpctrace} "${dst_path}" \
+    &> /dev/null
