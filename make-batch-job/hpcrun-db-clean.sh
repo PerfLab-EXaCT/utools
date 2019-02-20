da36dda882e7394c62502ac1917c7496b20b4adb
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2019-02-20 03:05:19 +0000

    update.
    git-svn-id: https://svn.pnl.gov/svn/hpcgroup/projects/modsim-tools@18850 4c623a4d-1e8e-0410-8a3e-ee3132d44425

--- make-batch-job/hpcrun-db-clean.sh
+++ make-batch-job/hpcrun-db-clean.sh
@@ -13,21 +13,27 @@
 
 toDir="0threads"
 
-cmd=$0
-
 #****************************************************************************
+
+scriptPath="${BASH_SOURCE[0]}" # works when script is sourced (unlike $0)
+scriptCmd=${scriptPath##*/} # cf. $(basename ...)
+
 
 if [[ $1 =~ -(-)?h(elp)? ]] ; then
   cat  <<EOF
-  usage: ${cmd} <hpctoolkit-measurements-dir>
+  usage: ${scriptCmd} <hpctoolkit-measurements-dir>
 
-  Retains only MPI rank data (moving thread data).
+  Retains only MPI rank data (moving thread data to '${toDir}').
 EOF
   exit
 fi
 
 
-arg_hpctkDir=$1
+#-----------------------------------------------------------
+#
+#-----------------------------------------------------------
+
+arg_hpctkDir="$1"
 
 if [[ ! -d ${arg_hpctkDir} ]] ; then
     printf "bad <hpctoolkit-measurements-dir>: '${arg_hpctkDir}'\n"
@@ -41,9 +47,9 @@ dst_path="${arg_hpctkDir}/${toDir}"
 
 mkdir -p "${dst_path}"
 
-mv "${src_path}"/*-??????-00[1-9]-*.{hpcrun,hpctrace}     "${dst_path}" \
+mv -n "${src_path}"/*-??????-00[1-9]-*.{hpcrun,hpctrace}     "${dst_path}" \
     &> /dev/null
-mv "${src_path}"/*-??????-0[1-9][0-9]-*.{hpcrun,hpctrace} "${dst_path}" \
+mv -n "${src_path}"/*-??????-0[1-9][0-9]-*.{hpcrun,hpctrace} "${dst_path}" \
     &> /dev/null
 
 num_hpcrun=$(ls -1 "${src_path}"/*.hpcrun | wc -l)
--- make-batch-job/hpcrun-db-prune.sh
+++ make-batch-job/hpcrun-db-prune.sh
@@ -11,53 +11,88 @@
 #
 #****************************************************************************
 
-i_ub="$1"
-i_stride="$2"
+toDir="0extra"
+
+do_dryrun=0
+
+#****************************************************************************
+
+scriptPath="${BASH_SOURCE[0]}" # works when script is sourced (unlike $0)
+scriptCmd=${scriptPath##*/} # cf. $(basename ...)
+
+if [[ $1 =~ -(-)?h(elp)? ]] ; then
+    cat  <<EOF
+  usage: ${scriptCmd} <hpctoolkit-measurements-dir> <upper-bound> <stride>
+
+  Prune MPI rank data in a strided fashion. Moves pruned data to '${toDir}'.
+EOF
+    exit
+fi
+
+
+if [[ $1 =~ -(-)dry(run)? ]] ; then
+    do_dryrun=1
+    shift
+fi
+
+
+#-----------------------------------------------------------
+#
+#-----------------------------------------------------------
+
+arg_hpctkDir="$1"
+
+if [[ ! -d ${arg_hpctkDir} ]] ; then
+    printf "bad <hpctoolkit-measurements-dir>: '${arg_hpctkDir}'\n"
+    exit 1
+fi
+
+#-----------------------------------------------------------
+#
+#-----------------------------------------------------------
+
+i_ub="$2"
+i_stride="$3"
 
 ((i_lb = i_stride - 1))
 
-todir="./xtra"
-
-#-----------------------------------------------------------
-#
-#-----------------------------------------------------------
-
-if [[ -z ${i_ub} -o -z ${i_stride} ]]; then
-  cat  <<EOF
-  usage: $0 <upper-bound> <stride>
-
-  Execute from within <hpctoolkit-measurements-dir>
-EOF
-
-  exit 1
+if [[ -z ${i_ub} || -z ${i_stride} ]]; then
+    printf "bad stride specification: upper: '${i_ub}'; stride: '${i_stride}'\n"
+    exit 1
 fi
 
-mkdir -p "${todir}"
+#****************************************************************************
 
-if test ! -d "${todir}" ; then
-  echo "target directory '${todir}' does not exist"
-  exit 1
-fi
+src_path="${arg_hpctkDir}"
+dst_path="${arg_hpctkDir}/${toDir}"
 
-echo "Moving hpcrun data from ranks ${i_lb}--${i_ub}:${i_stride} to '${todir}'."
+mkdir -p "${dst_path}"
 
-#-----------------------------------------------------------
-#
-#-----------------------------------------------------------
+printf "${arg_hpctkDir}: Pruning data from ranks ${i_lb}-${i_ub}:${i_stride}\n"
 
 for (( i = i_lb; i <= i_ub; i = i + i_stride )) ; do
-   path="."
-   pid=`printf "%06d" ${i}`
-   fnm_glob="${path}/*-${pid}-000-*.{hpcrun,log}"
+   pid=$(printf "%06d" ${i})
+   fnm_glob="*-${pid}-000-*.{hpcrun,hpctrace,log}"
 
-   unset did_find
-   for fnm in `eval ls -1 ${fnm_glob} 2> /dev/null` ; do
-     did_find="yes"
-     echo "moving: ${fnm} -> ${todir}/"
-     mv -f "${fnm}" "${todir}"/
+   fileL=$(eval ls -1 "${src_path}"/${fnm_glob} 2> /dev/null)
+   #fileL=$(eval compgen -G -- "${src_path}"/${fnm_glob} 2> /dev/null)
+   #printf "list: ${fileL}\n"
+      
+   for fnm in ${fileL} ; do
+       fnm_base=${fnm##*/} # cf. $(basename ...)
+       printf "move: ${fnm_base} -> ${toDir}\n"
+       if (( ! ${do_dryrun} )) ; then
+	   \mv -n "${fnm}" "${dst_path}"
+       fi
    done
-
-   if test -z "${did_find}" ; then
-     echo "skipping: ${fnm_glob}"
+   
+   if [[ -z ${fileL} ]] ; then
+       printf "skip: ${fnm_glob}\n"
    fi
+
+   # if mv -n "${src_path}"/${fnm_glob} "${dst_path}" 2> /dev/null ; then
+   #     printf "move: ${fnm_glob} -> ${dst_path}\n"
+   # else
+   #     printf "skip: ${fnm_glob}\n"
+   # fi
 done
