e9c4d05d06e60f85c8b7c4b5fdd82957165ec4fb
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2020-10-17 13:54:37 -0700

    'update'

--- plot-csv/streambench-lat.py
+++ plot-csv/streambench-lat.py
@@ -11,6 +11,18 @@ data = np.array([[0.062882,0.050042,8.707326,1.974689],
                  [0.081665,0.067247,14.099066,8.565123]])
 sns.set_theme()
 sns.set(font_scale=2)
-ax = sns.heatmap(data, xticklabels=nodes2, yticklabels=nodes, cmap='coolwarm', annot=True, fmt=".2f", annot_kws={"size":20})
-plt.title("STREAM Average Latency (s)", fontsize=20, fontweight='bold')
+
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(5.0, 2.5))
+
+ax = sns.heatmap(data, ax=axes,
+                 xticklabels=nodes2, yticklabels=nodes,
+                 cmap='RdBu_r', annot=True, fmt=".2f", annot_kws={"size":20})
+                 # cmap='coolwarm'
+
+ax.set_title("STREAM Average Latency (s)", fontsize=20, fontweight='bold')
+
+fig.tight_layout(pad=0.0, h_pad=0.0, w_pad=0.0)
+
+fig.savefig('my-file-name.pdf', bbox_inches='tight')
+
 plt.show()
